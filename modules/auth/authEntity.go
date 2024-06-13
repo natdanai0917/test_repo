cdbe04662acf942811c5e9da1397e3262bc503bf
natdanai0917 <natdanai.loh@gmail.com>
2024-06-13 23:06:18 +0700

    implement model,entity file of auth, item, player module and implement paginate in models

--- modules/auth/authEntity.go
+++ modules/auth/authEntity.go
@@ -1,1 +1,24 @@
 package auth
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type (
+	Credential struct {
+		Id          primitive.ObjectID `json:"_id" bson:"_id,omitempty"`
+		PlayerId    string             `json:"player_id" bson:"player_id"`
+		RoleCode    string             `json:"role_code" bson:"role_code"`
+		AccessToken string             `json:"access_token" bson:"access_token"`
+		CreatedAt   time.Time          `json:"created_at" bson:"created_at"`
+		UpdatedAt   time.Time          `json:"updated_at" bson:"updated_at"`
+	}
+
+	Role struct {
+		Id    primitive.ObjectID `json:"_id" bson:"_id,omitempty"`
+		Title string             `json:"title" bson:"title"`
+		Code  string             `json:"code" bson:"code"`
+	}
+)
--- modules/item/itemModel.go
+++ modules/item/itemModel.go
@@ -1,1 +1,36 @@
 package item
+
+import "github.com/natdanai0917/test_repo/modules/models"
+
+type (
+	CreateItemReq struct {
+		Title    string  `json:"title" validate:"required,max=64"`
+		Price    float64 `json:"price" validate:"required"`
+		ImageUrl string  `json:"image_url" validate:"required,max=255"`
+		Damage   int     `json:"damage" validate:"required"`
+	}
+
+	ItemShowCase struct {
+		ItemId   string  `json:"item_id"`
+		Title    string  `json:"title"`
+		Price    float64 `json:"price"`
+		Damage   int     `json:"damage"`
+		ImageUrl string  `json:"image_url"`
+	}
+
+	ItemSearchReq struct {
+		Title string `json:"title"`
+		models.PaginateReq
+	}
+
+	ItemUpdateReq struct {
+		Title    string  `json:"title" validate:"required,max=64"`
+		Price    float64 `json:"price" validate:"required"`
+		ImageUrl string  `json:"image_url" validate:"required,max=255"`
+		Damage   int     `json:"damage" validate:"required"`
+	}
+
+	EnableOrDisableItemReq struct {
+		Status bool `json:"status"`
+	}
+)
