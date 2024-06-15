23d55623a7dba3981172d5e3a241d55db0e9f8e5
natdanai0917 <natdanai.loh@gmail.com>
2024-06-15 21:55:19 +0700

    Implement Entity and Model file of payment, inventory modules | remove paymentEntity file

--- modules/inventory/inventoryModel.go
+++ modules/inventory/inventoryModel.go
@@ -1,1 +1,23 @@
 package inventory
+
+import (
+	"github.com/natdanai0917/test_repo/modules/item"
+	"github.com/natdanai0917/test_repo/modules/models"
+)
+
+type (
+	UpdateInventoryReq struct {
+		PlayerId string `json:"playerId" validate:"required,max=64"`
+		ItemId   string `json:"item_id" validate:"required,max=64"`
+	}
+
+	ItemInventory struct {
+		InventoryId string `json:"inventory_id"`
+		*item.ItemShowCase
+	}
+
+	PlayerInventory struct {
+		PlayerId string `json:"player_id"`
+		*models.PaginateRes
+	}
+)
--- modules/payment/paymentModel.go
+++ modules/payment/paymentModel.go
@@ -1,1 +1,11 @@
 package payment
+
+type (
+	ItemServiceReq struct {
+	}
+
+	ItemServiceReqDatum struct {
+		ItemId string  `json:"_id" validate:"required,max=64"`
+		Price  float64 `json:"price"`
+	}
+)
