0ac7d8dc9ab8f6166acabd2ef90cfe1bf0135695
natdanai0917 <natdanai.loh@gmail.com>
2024-08-18 13:14:31 +0700

    remove bson from CredentialRes

--- modules/auth/authModel.go
+++ modules/auth/authModel.go
@@ -27,11 +27,11 @@ type (
 	}
 
 	CredentialRes struct {
-		Id          string    `json:"_id" bson:"_id,omitempty"`
-		PlayerId    string    `json:"player_id" bson:"player_id"`
-		RoleCode    string    `json:"role_code" bson:"role_code"`
-		AccessToken string    `json:"access_token" bson:"access_token"`
-		CreatedAt   time.Time `json:"created_at" bson:"created_at"`
-		UpdatedAt   time.Time `json:"updated_at" bson:"updated_at"`
+		Id          string    `json:"_id"`
+		PlayerId    string    `json:"player_id"`
+		RoleCode    string    `json:"role_code"`
+		AccessToken string    `json:"access_token"`
+		CreatedAt   time.Time `json:"created_at"`
+		UpdatedAt   time.Time `json:"updated_at"`
 	}
 )
